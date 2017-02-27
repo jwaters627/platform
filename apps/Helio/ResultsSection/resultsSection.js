f0c3c9c0e83bd72c6b24c39c6c149a18054c6cd8
Jamie Waters <jwaters@crimsonhexagon.com>
2017-02-27 16:01:44 -0500

    sticky bar is now the nav structure to get to the different sections

--- apps/Helio/ResultsSection/resultsSection.js
+++ apps/Helio/ResultsSection/resultsSection.js
@@ -2,15 +2,16 @@ import React from 'react';
 import DownCarat from 'react-material-icons/icons/navigation/expand-more';
 import UpCarat from 'react-material-icons/icons/navigation/expand-less';
 import styles from './resultsSection.scss';
+import {Element} from 'react-scroll';
 
 class ResultsSection extends React.Component {
 	 constructor(props) {
         super(props);
         this.state = {
-            sections:[{id:1, title: 'Where is the conversation?', image: '../../../common/img/whereIsTheConvoScreenShot.png', collapsed: false},
-            {id: 2, title: 'What are people saying?', image: '../../../common/img/whatArePeopleSayingScreenShot.png', collapsed: false},
-            {id: 3, title: 'How are people feeling?', image: '../../../common/img/howArePeopleFeelingScreenShot.png', collapsed: false},
-            {id: 4, title: 'Who is the audience?', image: '../../../common/img/whoIsTheAudienceScreenShot.png', collapsed: false},
+            sections:[{id:1, title: 'Where is the conversation?', name: 'where', image: '../../../common/img/whereIsTheConvoScreenShot.png', collapsed: false},
+            {id: 2, title: 'What are people saying?', name: 'what', image: '../../../common/img/whatArePeopleSayingScreenShot.png', collapsed: false},
+            {id: 3, title: 'How are people feeling?', name: 'how', image: '../../../common/img/howArePeopleFeelingScreenShot.png', collapsed: false},
+            {id: 4, title: 'Who is the audience?', name: 'who', image: '../../../common/img/whoIsTheAudienceScreenShot.png', collapsed: false},
             ],
             sectionsBritain:[{id:1, title: 'Where is the conversation?', image: '../../../common/img/whereIsTheConvoScreenShot.png', collapsed: false},
             {id: 2, title: 'What are people saying?', image: '../../../common/img/whatArePeopleSayingScreenShot.png', collapsed: false},
@@ -30,13 +31,15 @@ class ResultsSection extends React.Component {
     		carat = (<UpCarat onClick={ () => { this.hideSection( item )}} style={{'display': 'inline-block', 'float': 'right', 'marginTop': '20px', 'cursor': 'pointer'}}/>)
     	}
     	return(
-    		<div className='suggestedSearchesContainer'>
-		    	<div className='contentContainer'>
-			   		<h3 style={{'fontSize': '44px', 'display': 'inline-block', 'marginTop': '0px'}}>{item.title}</h3>
-			   		{carat}
-			   		<img src={item.image} style={{'width': '100%', 'display': shown, 'marginTop': '32px'}}/>
-				</div>
-			</div>
+        		<div className='suggestedSearchesContainer'>
+                    <Element name={item.name} className='element'>
+    		    	<div className='contentContainer'>
+    			   		<h3 style={{'fontSize': '44px', 'display': 'inline-block', 'marginTop': '0px'}}>{item.title}</h3>
+    			   		{carat}
+    			   		<img src={item.image} style={{'width': '100%', 'display': shown, 'marginTop': '32px'}}/>
+    				</div>
+                    </Element>
+    			</div>
 		)
     }
 
--- apps/Helio/SourceBar/sourceBar.js
+++ apps/Helio/SourceBar/sourceBar.js
@@ -1,34 +1,48 @@
 import React from 'react';
 
 import styles from './sourceBar.scss';
+import classNames from 'classnames';
 import Share from 'react-material-icons/icons/social/share';
+import { Scroll, Link, scrollSpy } from 'react-scroll';
 import { StickyContainer, Sticky } from 'react-sticky';
 
 class FilterBar extends React.Component {
 	constructor(props) {
         super(props);
         this.state = {
-           
+           sections: [{text: 'WHERE',name: 'where', selected: true},{text: 'WHAT',name: 'what', selected: false},{text: 'HOW',name: 'how', selected: false},{text: 'WHO',name: 'who', selected: false}, ]
         }
+        this.handleSetActive = this.handleSetActive.bind(this);
+        this.renderSections = this.renderSections.bind(this);
     }
 
 
+    renderSections(item){
+    	let myClass = classNames(
+            'sourceTitle',
+            {
+                "selected": (item.selected),
+            }
+        );
+    	return (
+    		<div className="sourceContainer">
+				<Link className="sourceTitle" activeClass="selected" to={item.name} isDynamic={true} offset={-50} spy={true} smooth={true} duration={500} onSetActive={this.handleSetActive} onSetInactive={this.handleSetActive}>{item.text}</Link>
+			</div>
+    	)
+    }
+
+    handleSetActive(to){
+    	console.log(to);
+    }
+
+    
+
 	render(){
+		
 		return(
 				<Sticky>
 					<div className='sourceBar'>
-					    <div className="sourceContainer">
-					    	<p style={{'borderBottom': '4px solid #E06D20'}} className="sourceTitle">ALL SOURCES</p>
-					    </div>
-					    <div className="sourceContainer">
-					    	<p className="sourceTitle">TWITTER</p>
-					    </div>
-					    <div className="sourceContainer">
-					    	<p className="sourceTitle">INSTAGRAM</p>
-					    </div>
-					    <div className="sourceContainer">
-					    	<p className="sourceTitle">FACEBOOK</p>
-					    </div>
+						{this.state.sections.map(this.renderSections)}
 					    <Share style={{'boxShadow': '0px 2px 8px rgba(30, 30, 30, 0.7)' ,'display': 'inline-block', 'float': 'right', 'borderRadius': '24px', 'padding': '10px', 'marginRight': '76px', 'marginTop': '34px', 'color': '#fff', 'backgroundColor': '#E06D20'}}/>
 				  	</div>
 			  	</Sticky>
